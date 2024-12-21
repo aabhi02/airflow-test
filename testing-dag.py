9897b931974918601e0e71d647d7fd1ff893345e
Abhishek A <120622721+aabhi02@users.noreply.github.com>
2024-12-22 02:28:02 +0530

    Update testing-dag.py

--- testing-dag.py
+++ testing-dag.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'abhishek',
@@ -8,15 +8,20 @@ default_args = {
     'retry_deply': timedelta(minutes=2)
 }
 
+
+def testing():
+    print("nkn banthu!!")
+
+
 with DAG(
     dag_id="test1",
     description="tseing dag",
     schedule_interval='@daily',
-    start_date=datetime(2024,12,22,1)
+    start_date=datetime(2024,12,21,1)
 ) as dag:
-    task1 = BashOperator(
+    task1 = PythonOperator(
         task_id = "task1",
-        bash_command = "echo nkn banthu!!"
+        python_callable=testing
     )
 
 
